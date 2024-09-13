2920b299edf4032a2da76252e30d6bb7760d1c79
Syed Shahir Ahmed Rakin <38401561+syedrakin707@users.noreply.github.com>
2024-09-14 03:59:42 +0600

    Included Comments before each function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// Function 1: Finding the Difference between two Numbers
 function calculateDifference(num1, num2){
     return Math.abs(num1-num2)
 }
-
+// Function 2: Checking whether a number is Odd or Not
 function isOdd(num){
     if (num%2==1){
         return true
@@ -10,10 +11,12 @@ function isOdd(num){
     }
 }
 
+// Function 3: Finding the Minimum Number in an Array
 function findMin(arr){
     return Math.min(...arr)
 }
 
+// Function 4: Returning the Array with Even Numbers Only
 function filterEvenNumbers(arr){
     var result = arr.filter(function(item){
         return (item % 2 == 0);
@@ -21,16 +24,19 @@ function filterEvenNumbers(arr){
     return result
 }
 
+// Function 5: Taking an Array and sort them to a Descending Order
 function sortArrayDescending(arr){
     var sorted = arr.toSorted();
     var descSorted = sorted.toReversed();
     return descSorted;
 }
 
+// Function 6: Returning the string with just the first letter converted into Lower Case
 function lowercaseFirstLetter(string){
     return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
+// Function 7: Returning the number of vowels in a String
 function countVowels(string){
     vowels_for_test = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     var v_count = 0; var i;
@@ -42,6 +48,7 @@ function countVowels(string){
     return v_count;
 }
 
+// Function 8: Returning the Average of the elements in an Array
 function findAverage(array){
     var sum = 0; var j;
     for (j = 0; j < array.length; j++) {
@@ -51,7 +58,6 @@ function findAverage(array){
 }
 
 // Did a test drive on the functions to ensure that I am on the right path
-
 var diff = calculateDifference(3.5, 6.0);
 console.log(diff)
 
